edbc6333d2e23775e4902e59e0f73547f1e924d7
Helder Patrick <helderpatrick3@gmail.com>
2026-06-24 22:49:18 -0300

    conclusão aula 3

--- exercicios/exe4.py
+++ exercicios/exe4.py
@@ -1,3 +1,8 @@
 # Faça um programa que receba um número inteiro e calcule sua raiz quadrada e exiba o resultado.
 
-numero = input("Entre com um numero inteiro!")
+numero = input("Entre com um numero inteiro para calcular a sua raiz quadrada : ")
+numero = int(numero)
+
+raiz = numero ** (1/2) # numero ** 0.5
+raiz = round(raiz,4)
+print("Raiz quadrada de", numero, "é", raiz)
---
+++ exercicios/exe5.py
@@ -0,0 +1,7 @@
+# Faça um programa que exiba o dobro de um número inserido pelo usuário.
+
+
+numero = input("digite um número : ")
+numero = int(numero)
+dobro = numero * 2
+print("o dobro de ",numero,"é",dobro )
